58449854100568f17102d5aa507740860ad0f3f3
Gua1diaN <53200684+Gua1diaN@users.noreply.github.com>
2021-01-16 16:30:34 +1100

    init
    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  InputGroup,
+  Form,
+  Row,
+  Col
+} from 'react-bootstrap';
 
-function App() {
+const App = () => {
+  const formControlRef = useRef();
+  const [list, setList] = useState([
+    {name:'first'},
+    {name:"second"}
+  ]);
+
+  const onSubmit = (e) => {
+    console.log(e);
+    let newList = [
+      ...list,
+      {
+        name:e
+      }
+    ];
+    setList(newList)
+
+  };
+
+  const removeEl = (index) => {
+    let newList = [...list];
+    newList.splice(index,1);
+    setList(newList);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <label htmlFor="basic-url">Deposited Plan</label>
+      <InputGroup className="mb-3">
+      <InputGroup.Prepend>
+        <InputGroup.Text id="basic-addon3">
+          DP
+        </InputGroup.Text>
+      </InputGroup.Prepend>
+      <Row  className='align-items-center' noGutters>
+        {list.map((el, index) => {
+          return(
+            <Col sx="auto" className="p-2" key={index} onClick={()=>removeEl(index)}>
+                {el.name}
+            </Col>
+          );
+        })}
+      </Row>
+      <Col xs>
+        <Form
+          onSubmit={(e) =>{
+            e.preventDefault();
+            onSubmit(formControlRef.current.value);
+          }}
+          noValidate
         >
-          Learn React
-        </a>
-      </header>
+          <Form.Control
+            ref={formControlRef}
+            placeholder="Plan Number"
+          />
+        </Form>
+      </Col>
+    </InputGroup>
     </div>
   );
 }
